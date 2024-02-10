5ffbc26c313aaa11f91da928c0bcbf5f8318ac18
Kynuak <bapt.trouille@gmail.com>
2024-02-11 00:04:00 +0100

    PokemonCard Dynamique en fonction tableau et apparation de l'image si elle est défini ou pas

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,12 +1,32 @@
 function PokemonCard() {
+
+    const pokemonList = [
+        {
+            name: "Bulbizarre",
+            imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" 
+        },
+        {
+            name: "Mew",
+        }
+    ];
+
+    const pokemon = pokemonList[0];
+
+
+
     return (
     <figure className="cardPokemon">
-        <img 
-            src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" 
-            alt=""
+        {pokemon.imgSrc === undefined ? 
+            <p> ??? </p> 
+            : 
+            <img 
+            src={ pokemon.imgSrc }
+            alt={ pokemon.name }
             className="img-pokemon"
-        />
-        <figcaption className="name-pokemon">Bulbizarre</figcaption>
+            /> 
+        }
+
+        <figcaption className="name-pokemon">{ pokemon.name }</figcaption>
     </figure>
     
 )}
