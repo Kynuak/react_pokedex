0f58a59446cbb81e09975b0e21acb8c1d468cae5
Kynuak <bapt.trouille@gmail.com>
2024-02-10 23:45:35 +0100

    Création du composant PokemonCard

---
+++ src/components/PokemonCard.jsx
@@ -0,0 +1,14 @@
+function PokemonCard() {
+    return (
+    <figure className="cardPokemon">
+        <img 
+            src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" 
+            alt=""
+            className="img-pokemon"
+        />
+        <figcaption className="name-pokemon">Bulbizarre</figcaption>
+    </figure>
+    
+)}
+
+export default PokemonCard;
